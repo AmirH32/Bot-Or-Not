5ba76de999ea64312730b478100bb6027d87bff4
AmirH32 <amirhassanali2610@gmail.com>
2024-11-02 17:06:33 +0000

    Made the backend work

--- backend.py
+++ backend.py
@@ -1,45 +1,35 @@
 from flask import Flask, request, jsonify
-import requests
-import json
-
+from mistralai import Mistral
+import os
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
 
+api_key = os.environ["MY_API_KEY"]
+model = "mistral-large-latest"
 
-ARLIAI_API_KEY = "dec15259-a611-485c-871f-e2ee49a280da"
+client = Mistral(api_key=api_key)
 
-def prompt(data: dict):
-    url = "https://api.arliai.com/v1/chat/completions"
-    if not isinstance(data, dict):
-        raise TypeError("Expected 'data' to be a dictionary")
-    else: 
-        payload = json.dumps({
-        "model": "Mistral-Nemo-12B-Instruct-2407",
-        "messages": [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Hello!"},
-            {"role": "assistant", "content": "Hi!, how can I help you today?"},
-            {"role": "user", "content": "Say hello!"}
-        ],
-        "repetition_penalty": 1.1,
-        "temperature": 0.7,
-        "top_p": 0.9,
-        "top_k": 40,
-        "max_tokens": 1024,
-        "stream": True
-        })
-        headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f"Bearer {ARLIAI_API_KEY}"
-        }
+def prompt(data):
+    """
+    Argument: Takes in a prompt string
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-    # Extract and return the reply from the assistant
-    if response.status_code == 200:
-        return response.headers.get('Content-Type')
-    else: 
-        return "failed"
+    Returns: returns the output of the prompted string
+
+    Purpose: To take a prompt string, pass it to an AI via API and return the response
+    """
+
+    chat_response = client.chat.complete(
+            model = model,
+            messages = [
+                {
+                    "role": "user",
+                    "content": data,
+                },
+            ]
+        )
+    return chat_response.choices[0].message.content
 
 
 
@@ -49,13 +39,12 @@ def get_data():
 
 @app.route('/api/data', methods=['POST'])
 def post_data():
-    print("got request")
     data = request.json
     if data:
         # You can process the data here as needed
-        print(prompt(data))
+        response = prompt(data["prompt"])
         
-        return jsonify({"received": data}), 201  # Respond with the received data
+        return jsonify({"response": response}), 201  # Respond with the received data
     return jsonify({"error": "Invalid data"}), 400  # Bad request if data is not valid
 
 if __name__ == '__main__':
