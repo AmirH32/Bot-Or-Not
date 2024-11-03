dd68bb77624dd6f2bfe8708e0cf6455efc5f6536
AmirH32 <amirhassanali2610@gmail.com>
2024-11-03 14:40:10 +0000

    API key

--- Scripts/chat.gd
+++ Scripts/chat.gd
@@ -6,10 +6,8 @@ extends Node2D
 @onready var text_edit = $TextEdit       # Main TextEdit box
 @onready var answer_input = $AnswerEdit      # Input field (e.g., LineEdit or another TextEdit)
 @onready var http_request_node = $HTTPRequest # The HTTP Request node
-@onready var resume_timer = $Timer  # Reference to the Timer node
 var prompt_count: int = 0 # initialise prompt count
 var parent_ai : AI_robot
-var win
 
 signal death
 signal increment
@@ -23,7 +21,6 @@ func create_post_data(prompt, prompt_count):
 	
 # On startup get the first question from AI
 func _ready() -> void:
-	win = null
 	 # Set the TextEdit to full screen size
 	print(get_viewport().size)
 	
@@ -34,11 +31,6 @@ func _ready() -> void:
 	http_request_node.get_first_question()
 	prompt_count += 1
 	print("Number of prompts sent:", prompt_count)
-	
-	### Sets timer	
-	resume_timer.wait_time = 2.0  # Set the wait time to 2 seconds
-	resume_timer.one_shot = true    # Make it a one-shot timer
-	resume_timer.connect("timeout", _on_resume_timer_timeout())  # Connect the timeout signal
 	
 	
 # Function called when the Send button is pressed
@@ -97,15 +89,8 @@ func display_ai_response(response):
 			if response_string[response_length - 1] == "h" or "human" in response_string:
 				text_edit.text += "The bot has detected you are human, you lose!"
 				emit_signal("death")
-				win = false
 				parent_ai.resume(true)
 			else:
 				text_edit.text += "The bot has detected you are an AI, you win!"
 				emit_signal("increment")
-				win = true
 				parent_ai.resume(false)
-
-func _on_resume_timer_timeout():
-	# Determine if the AI should resume with true or false based on previous logic
-	var should_resume = win  # Change this based on your logic
-	parent_ai.resume(should_resume)  # Call resume after the timer
