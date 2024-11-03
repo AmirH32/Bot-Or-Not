2e9fac90c1affb8166f2975cacd174547d127f09
Benedict Orlich <benedict.orlich@gmail.com>
2024-11-03 14:05:43 +0000

    Add die and reset methods and implement win method

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,6 +1,8 @@
 extends Node
 
 
+var level: Node2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -12,12 +14,22 @@ func _process(_delta):
 
 
 func start_game(level_name: String):
-	var level: Node2D = load("res://scenes/levels/"+level_name+".tscn").instantiate()
+	level = load("res://scenes/levels/"+level_name+".tscn").instantiate()
 	var win_area: WinArea = level.get_node("WinArea")
 	win_area.win.connect(win)
 	add_child(level)
 	$UI.hide()
 
+func reset_level():
+	$UI.show()
+	remove_child(level)
+	level.queue_free()
 
-func win(num_bamboozled: int):
-	print(num_bamboozled)
+
+func win(_num_bamboozled: int):
+	reset_level()
+	$UI.win()
+
+func die():
+	reset_level()
+	$UI.die()
