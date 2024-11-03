82a92670d96a3dd115f68fc17c4ed6184c5e479a
Benedict Orlich <benedict.orlich@gmail.com>
2024-11-03 12:00:41 +0000

    Give UI any number of levels

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,13 +1,21 @@
 extends Control
 
 
+var levels := {
+	"Level 1": "level_1",
+	"Level 2": "level_2",
+	"Level 3": "level_3"
+}
+
+signal open_level(level_file_name: String)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	activate_level_select()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
@@ -24,3 +32,10 @@ func _on_survival_pressed():
 func _on_escape_pressed():
 	$GamemodeSelect.hide()
 	$LevelSelect.show()
+
+func activate_level_select():
+	for level_name in levels.keys():
+		var button = Button.new()
+		button.text = level_name
+		button.pressed.connect(open_level.emit.bind(levels[level_name]))
+		$LevelSelect/VBoxContainer/ScrollContainer/VBoxContainer.add_child(button)
