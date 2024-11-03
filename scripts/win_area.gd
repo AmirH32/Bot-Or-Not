d15f22caf56365c7aa102e298bd797208d68433f
Benedict Orlich <benedict.orlich@gmail.com>
2024-11-03 11:12:26 +0000

    Improve player tracking and start bamboozle tracking

--- scripts/win_area.gd
+++ scripts/win_area.gd
@@ -1,8 +1,10 @@
+class_name WinArea
 extends Area2D
 
 var enemy_in_area := false
+var player_in_area := false
 
-signal win
+signal win(num_bamboozled: int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,10 +20,15 @@ func _process(_delta: float):
 
 func _on_player_entered(_body: PhysicsBody2D):
 	enemy_in_area = false
+	player_in_area = true
 	await get_tree().create_timer(2.0).timeout
-	if not enemy_in_area:
-		win.emit()
+	if not enemy_in_area and player_in_area:
+		win.emit(0) # TODO: Add bamboozle tracking
 
 
 func _on_enemy_entered(_body: PhysicsBody2D):
 	enemy_in_area = true
+
+
+func _on_player_exited(body):
+	player_in_area = false
